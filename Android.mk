4a4608a8982ffc5d2612c9da4e20acb635e10061
MarijnS95 <marijns95@gmail.com>
2019-12-04 17:59:09 +0100

    Convert QcRilAm to SDK compliant app without PRIVATE_API access.
    In order to build devices on API 28 and above, all vendor apps need to
    use public, "stable" SDK APIs. Use of LOCAL_PRIVATE_PLATFORM_APIS is not
    allowed anymore.
    
    Noteworthy changes:
    - The app doesn't need to be privileged. Both permissions have
      protectionLevel=normal, and the selinux domain can be applied to
      non-privileged apps just fine.
    - AudioManager is a wrapper around AudioSystem, at least when it comes
      to get/setParameters which are used here.
    - Sim slot count now comes from SubscriptionManager, which internally
      uses the isub system service and needs an extra selinux permission to
      retrieve.
      Note that the Max() variant, which retrieves the number of sim slots,
      does not require the dangerous permission READ_PHONE_STATE, while the
      "actual inserted sim count" variant of this function does.
    - the android.hidl.manager dependency isn't necessary.
    
    Signed-off-by: MarijnS95 <marijns95@gmail.com>

--- Android.mk
+++ Android.mk
@@ -8,11 +8,9 @@ LOCAL_PROPRIETARY_MODULE := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_JAVA_LIBRARIES :=	android.hidl.manager-V1.0-java
 LOCAL_STATIC_JAVA_LIBRARIES := vendor.qti.hardware.radio.am-V1.0-java
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-LOCAL_PRIVATE_PLATFORM_APIS := true
-LOCAL_PRIVILEGED_MODULE := true
+LOCAL_SDK_VERSION := system_current
 
 include $(BUILD_PACKAGE)
